447fb7e0caee1887b0ff4da211095d168e7f698f
Sudarshan Sinha <sudarshan@Sudarshans-MacBook-Air.local>
2021-06-08 14:11:14 +0530

    Initial commit

---
+++ backend/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "SwiftBankDB";
+
+    $conn = mysqli_connect($servername,$username,$password,$db);
+
+    if (!$conn) {
+        die("Error!");
+    }
+
+
+
+
+
+?>
